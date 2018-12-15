ca58af32e968a6b39532047de77b30c1defe7b46
Joachim Roeleveld <joachimroeleveld@gmail.com>
2018-12-15 13:59:33 +0100

    Add morgan logger

--- src/framework/expressServer.js
+++ src/framework/expressServer.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bearerToken = require('express-bearer-token');
+const morgan = require('morgan');
 
 const middleware = require('./middleware');
+const { isProduction } = require('../shared/config');
 const { NotFoundError } = require('../shared/errors/index');
 const routes = require('../routes/index');
 
 function createExpressApp() {
   try {
     const app = express();
+
+    const logFormat = isProduction ? 'combined' : 'dev';
+    app.use(morgan(logFormat));
 
     app.set('view engine', 'hbs');
     app.set('views', './src/shared/templates');
