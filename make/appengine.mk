585c7962a987e28cb06770552c80ec06b344dee4
Joachim Roeleveld <joachimroeleveld@gmail.com>
2018-12-27 20:21:52 +0100

    Environment-specific app.yaml

--- make/appengine.mk
+++ make/appengine.mk
@@ -1,4 +1,4 @@
 .PHONY: appengine
 
 appengine-deploy: env
-	gcloud --quiet --project ${GCP_PROJECT_ID} app deploy app.yaml --promote --stop-previous-version --version="${version}"
+	gcloud --quiet --project ${GCP_PROJECT_ID} app deploy app.${_ENV}.yaml --promote --stop-previous-version --version="${version}"
