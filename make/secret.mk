138bee8ad7ec3d62759faeaf4220c355dbc5f136
Joachim Roeleveld <joachimroeleveld@gmail.com>
2019-03-04 21:16:11 +0100

    Fix tests

--- make/secret.mk
+++ make/secret.mk
@@ -1,10 +1,10 @@
 .PHONY: secret
 
-KMS_ARGS=--project=$(GCP_PROJECT_ID) --location=$(GCP_REGION) --keyring=$(SERVICE_NAME) --key=secret-conf --ciphertext-file=secret.$(env).enc --plaintext-file=-
+KMS_ARGS=--location=$(GCP_REGION) --keyring=$(SERVICE_NAME) --key=secret-conf --ciphertext-file=secret.$(env).enc --plaintext-file=-
 
 secret-encrypt: env
-	tar cvf - $(SECRET_DIR)| gcloud kms encrypt $(KMS_ARGS)
+	tar cvf - $(SECRET_DIR)| gcloud --project=$(GCP_PROJECT_ID) kms encrypt $(KMS_ARGS)
 
 secret-decrypt: env
-	gcloud kms decrypt $(KMS_ARGS) | tar vxf  -
+	gcloud --project=$(GCP_PROJECT_ID) kms decrypt $(KMS_ARGS) | tar vxf  -
 
