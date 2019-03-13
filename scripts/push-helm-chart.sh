0f296c27b8052fcb73d3fbad87af595efb8cdadc
Joachim Roeleveld <joachimroeleveld@gmail.com>
2019-03-13 16:33:57 +0100

    Fixup

--- scripts/push-helm-chart.sh
+++ scripts/push-helm-chart.sh
@@ -6,4 +6,4 @@ helm init --client-only
 helm plugin install https://github.com/nouney/helm-gcs
 helm repo add repo $REPO
 helm package infra/helm/api --version=$CHART_VERSION
-helm gcs push infra/helm/api-$CHART_VERSION.tgz repo
+helm gcs push api-$CHART_VERSION.tgz repo
